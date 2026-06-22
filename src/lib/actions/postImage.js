10223e968b66a6c9412fa2465fda2f98af1d3860
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-06-22 13:59:30 +0200

    bug: make a formData object of the image file for imgbb api

--- src/lib/actions/postImage.js
+++ src/lib/actions/postImage.js
@@ -1,15 +1,15 @@
+"use server";
 export const postImage = async (imageFile) => {
     try {
+        const formData = new FormData();
+        formData.append("image", imageFile);
         const imageResponse = await fetch(`${process.env.IMAGE_API}?key=${process.env.IMAGE_API_KEY}`, {
             method: "POST",
-            headers: {
-                "Content-type": "image/*"
-            },
-            body: imageFile,
+            body: formData,
         });
 
-        const imageURL = await imageResponse.json()?.data?.url;
-
+        const result = await imageResponse.json();
+        const imageURL = result?.data?.url;
         console.log(imageURL);
 
         return imageURL;
