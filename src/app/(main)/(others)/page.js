6ede5ef7ee1e6741bd1b5174b5632afb2f9623a8
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-06-27 12:43:11 +0200

    deployment check

--- src/app/(main)/(others)/page.js
+++ src/app/(main)/(others)/page.js
@@ -2,6 +2,7 @@ import { Banner } from "@/components/home/Banner";
 import Stats from "@/components/home/Stats";
 
 export default async function Home() {
+  console.log(`${process.env.NEXT_PUBLIC_SERVER_URL}`);
   return (
     <>
       <Banner></Banner>
